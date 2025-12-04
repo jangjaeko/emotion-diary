fdab5fcebbeaffbd59839d26fd7bd6004ddcc825
unknown <jangjaeko45@gmail.com>
2025-12-03 19:18:03 -0800

    feat : managing diary features

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { getEmotionImage } from "./util/get-emotion-image";
 import Button from "./components/button";
 import Header from "./components/Header";
 
-import { useReducer } from "react";
+import { useReducer, useRef, createContext } from "react";
 //1. "/" : Home for Emotion Diary
 //2. "/new" : write new diary
 //3. "/dirary" : viewing diary contents
@@ -31,12 +31,58 @@ const tempData = [
   },
 ];
 
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
 function reducer(state, action) {
-  return state;
+  switch (action.type) {
+    case "CREATE": {
+      return [action.data, ...state];
+    }
+    case "UPDATE": {
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    }
+    case "DELETE": {
+      return state.filter(
+        (item) => String(item.id) !== String(action.targetId)
+      );
+    }
+    default:
+      return state;
+  }
 }
 
 function App() {
   const [data, dispatch] = useReducer(reducer, tempData);
+  const dataId = useRef(3);
+  //add diary
+  const onCreate = (createdDate, emotionid, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current++,
+        createdDate: new Date().getTime(),
+        emotionid,
+        content,
+      },
+    });
+  };
+  // edit diary
+  const onUpdate = (id, createdDate, emotionid, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: { id, createdDate, emotionid, content },
+    });
+  };
+  // delete diary
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      targetId: id,
+    });
+  };
 
   return (
     <>
@@ -46,14 +92,18 @@ function App() {
         rightChild={<Button text={"Right"} />}
       />
       <Button /> */}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <button onClick={onDelete(1)}>TEST BUTTON</button>
+      <DiaryStateContext value={data}>
+        <DiaryDispatchContext value={(onCreate, onDelete, onUpdate)}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext>
+      </DiaryStateContext>
     </>
   );
 }
